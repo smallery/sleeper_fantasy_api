26ce5cd2a5a035175325f0b71d9534a0c62614c5
Samuel Mallery <43734581+smallery@users.noreply.github.com>
2024-09-03 13:55:53 -0400

    feat: added potential for user to specify result conversion

--- sleeper_api/config.py
+++ sleeper_api/config.py
@@ -7,6 +7,29 @@ BASE_URL = "https://api.sleeper.app/v1/"
 # True = convert everything to object oriented version
 # False = return raw json results with no object oriented conversion
 CONVERT_RESULTS = True
+# TO DO: implement way for package user to specify this:
+# ex:
+# class Config:
+#     def __init__(self):
+#         # Default configuration
+#         self.CONVERT_RESULTS = True
+
+#     def set_convert_results(self, value: bool):
+#         self.CONVERT_RESULTS = value
+
+#     def get_convert_results(self) -> bool:
+#         return self.CONVERT_RESULTS
+
+# # Create a global config instance
+# config = Config()
+# then in the files, instead of importing CONVERT_RESULTS I can instead:
+# import it: from sleeper_fantasy_api.config import config
+# reference it: result = config.get_convert_results()
+
+# user could specify globally by doing something like:
+# from sleeper_fantasy_api.config import config
+# config.set_convert_results(False)
+
 
 from datetime import datetime
 DEFAULT_SEASON = datetime.now().year
